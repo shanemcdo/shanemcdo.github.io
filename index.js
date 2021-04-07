0bfdefaf832e38445c0e50fb160b74477cb1c5e2
Shane McDonough <shanemcdman@gmail.com>
2021-04-06 18:50:39 -0700

    Only use public information

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const API_TOKEN = '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';
 const age = document.querySelector('#age-value');
-const private_repos = document.querySelector('#private-repos-value');
 const public_repos = document.querySelector('#public-repos-value');
+const github_age = document.querySelector('#github-age-value');
+
 setInterval(()=>{
     let birth = new Date(2002, 2, 1);
     let now =  Date.now();
     age.innerHTML = ((now - birth) / (1000 * 60 * 60 * 24 * 365.2425)).toFixed(9);
 }, 50);
 
-fetch('https://api.github.com/user', {
-    headers: {
-        Authorization: 'Basic ' + btoa('kermitpurple:' + API_TOKEN),
-    }
-}).then(res=>res.json()).then(res=>{
+fetch('https://api.github.com/users/kermitpurple').then(res=>res.json()).then(res=>{
     console.log(res);
-    private_repos.innerHTML = res.owned_private_repos;
+    let creation_date = new Date(res.created_at);
     public_repos.innerHTML = res.public_repos;
+    setInterval(()=>{
+        github_age.innerHTML = ((Date.now() - creation_date) / (1000 * 60 * 60 * 24 * 365.2425)).toFixed(9);
+    }, 50);
 });
