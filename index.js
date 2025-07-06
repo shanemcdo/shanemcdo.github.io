b5309ae0fec9927d873585e84d57ef8f8e41e7d8
Shane McDonough <shanemcdman@gmail.com>
2025-07-05 19:25:34 -0700

    create and use link search

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const age = document.querySelector('#age-value');
 const public_repos = document.querySelector('#public-repos-value');
 const github_age = document.querySelector('#github-age-value');
+const link_search = document.querySelector('#link-search');
+const site_links = document.querySelectorAll('.site-link');
 
 setInterval(()=>{
     let birth = new Date(2002, 2, 1);
@@ -14,4 +16,16 @@ fetch('https://api.github.com/users/shanemcdo').then(res=>res.json()).then(res=>
     setInterval(()=>{
         github_age.innerHTML = ((Date.now() - creation_date) / (1000 * 60 * 60 * 24 * 365.2425)).toFixed(9);
     }, 50);
+});
+
+link_search.addEventListener('change', ()=>{
+    if(link_search.value === ''){
+        for (const link of site_links) {
+            link.style.display = '';
+        }
+    }else{
+        for (const link of site_links) {
+            link.style.display = link.textContent.toLowerCase().includes(link_search.value.toLowerCase()) ? '' : 'none';
+        }
+    }
 });
